3c042104b809e3533bb66d156a6b36d1e7bbf835
Zach Liou <zachliou@gmail.com>
2016-07-03 13:57:23 -0700

    Add show(values) for easy displaying

--- attendance.py
+++ attendance.py
@@ -5,23 +5,58 @@
 from sys import argv
 
 
-# Functions
+# Functions ##########
 
+
+# Prints usage
 def print_usage():
 	print "Usage: python attendance.py input_file"
 
 
-# Check args
+# Prints values in organized rows, separated by spaces
+# @param values		A list of values to print
+def show(values):
+	
+	# Constants
+	NAMES_PER_LINE = 3;
+	TAB_WIDTH = 8
+	SPACING = NAMES_PER_LINE * TAB_WIDTH
+
+	# Init vars
+	row = ""
+	num_printed = 0;
+
+	# Loop through each value
+	for value in values:
+
+		# Add value to row and increment counter
+		row += value 
+		num_printed += 1
+
+		# Print spaces for spacing
+		num_spaces = SPACING - len(value)
+		if num_spaces < 1:
+			print " "
+		for i in range (0, num_spaces):
+			row += ' '
+		if (num_printed % NAMES_PER_LINE) == 0:
+			print row
+			row = ""
+
+
+# Check args ##########
 if len(argv) != 2:
-	print "Incorrect number of arguments."
+	print "\nIncorrect number of arguments."
 	print_usage()
+	print ""
 	exit();
 
 # Read infile line-by-line, and remove newlines
-absent = [line.rstrip('\n') for line in open(argv[1])]
+raw = [line.rstrip('\n') for line in open(argv[1])]
+roster = [name.replace('\t', ' ') for name in raw]
 
-print absent
 
+show(roster);
 
 
 
